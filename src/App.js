74adf1d2999cf0ab7e9984d324d8df2f16bb7c7e
hikkary <hikkary@MacBook-Pro-de-Zakaria.local>
2019-11-14 18:03:41 +0100

    update course

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import posed from 'react-pose';
 
-function App() {
+import styled from 'styled-components';
+class App extends React.Component{
+
+  state={
+    animation:'start'
+  }
+
+  componentDidMount(){
+    this.interval = setInterval(() => {
+      this.setState({animation: this.state.animation === 'start' ? 'end' : 'start'});
+    }, 1000);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.interval);
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Lambda color="green"/>
+      <Lambda />
+      <LambdaTheme color="red"></LambdaTheme>
+      <LambdaTheme color="orange"></LambdaTheme>
+      <LambdaTheme color="green"></LambdaTheme>
+      <LambdaTheme ></LambdaTheme>
+      Animation
+      <LambdaAnimated initialPose="start" pose="end"></LambdaAnimated>
+      <LambdaAnimated pose={this.state.animation}></LambdaAnimated>
     </div>
   );
 }
+
+  }
+
+const Lambda = styled.div`
+  margin:20px;
+  height: 50px;
+  width: 50px;
+  background-color: ${props => props.color ? props.color : props.theme.primary};
+`;
+
+const LambdaAnimated = posed(Lambda)({
+  start: {x: 0, transition: {duration: 1000}},
+  end: {x: 100, transition: {duration: 1000}},
+});
+
+const LambdaTheme = styled.div`
+  margin:20px;
+  height: 50px;
+  width: 50px;
+  background-color: ${props => props.color ?  props.theme.fireColors[props.color]  : props.theme.fireColors.red};
+`;
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {ThemeProvider} from 'styled-components';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = {
+  primary: 'blue',
+  fireColors: {
+    red: 'red',
+    green: 'green',
+    orange: 'orange'
+  }
+};
+
+class Index extends React.Component{
+  render(){
+    return(
+    <ThemeProvider theme={theme}>
+       <App />
+    </ThemeProvider>
+    );
+  }
+}
+
+ReactDOM.render(<Index></Index>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
